4b8d91ad77e7d51f4c124456a44f495c1ad8a643
Lyliana Sousa <81655878+LylianaSousa@users.noreply.github.com>
2021-09-19 10:27:42 -0300

    Add files via upload
    Atividade criando um APP simples de cadastro de séries em .NET desenvolvido durante o Bootcamp Decola Tech da Avanade em parceria com a DIO.

---
+++ CadastrodeSéries/CLasses/Entidadebase.cs
@@ -0,0 +1,7 @@
+namespace CadastrodeSéries
+{
+    public abstract class Entidadebase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ CadastrodeSéries/Enum/Genero.cs
@@ -0,0 +1,22 @@
+namespace CadastrodeSéries
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Documentario = 4,
+		Drama = 5,
+		Espionagem = 6,
+		Faroeste = 7,
+		Fantasia = 8,
+		Ficcao_Cientifica = 9,
+		Musical = 10,
+		Romance = 11,
+		Suspense = 12,
+		Terror = 13,
+		Dorama = 14,
+		Cinema_Brasileiro = 15,
+		Cinema_Latino_americano =16,
+	}
+}
